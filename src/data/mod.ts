6223d826ca9703509394dd667629f1e3e2fb8e8f
sieve <68281563+s-i-e-v-e@users.noreply.github.com>
2024-04-25 21:19:04 +0000

    update dependencies

---
+++ src/data/mod.ts
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2024 Sieve (https://github.com/s-i-e-v-e)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+export * from "./bs.ts";
+export * from "./cs.ts";
+export * from "./text.ts";
+export * from "./ts.ts";
+export * from "./xml.ts";
